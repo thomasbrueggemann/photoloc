ee5bc6f355646251f5c33bd45d540f2cd47cf7e2
Thomas <thomas.brueggemann@maersk.com>
2022-05-29 01:27:34 +0200

    start with persistence

--- src/app/json-storage.ts
+++ src/app/json-storage.ts
@@ -1,3 +1,8 @@
-export function persist<T>(key: string, obj: T) {
+export function persistObject<T>(key: string, obj: T) {
+    localStorage.setItem(key, JSON.stringify(obj));
+}
 
+export function readObject<T>(key: string): T {
+    const value = localStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
 }
